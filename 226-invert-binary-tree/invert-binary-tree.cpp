aa993417232f5efbc210b66ff57310c786a821bb
Astha Singh <aasthasingh.er@gmail.com>
2026-08-10 14:57:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (26.71%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -9,22 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+//..................  APP-1...........................
+
+// class Solution {
+// public:
+// TreeNode* DFS(TreeNode* root){
+//     if(root==NULL)
+//     return NULL;
+//     TreeNode* L=DFS(root->left);
+//     TreeNode* R=DFS(root->right);
+
+//     root->left=R;
+//     root->right=L;
+//     return root;
+// }
+//     TreeNode* invertTree(TreeNode* root) {
+//         if(root==NULL)
+//         return NULL;
+//         DFS(root);
+//         return root;
+//     }
+// };
+
+
+
+
+
+
+
+// .............................APP-2.....................
 class Solution {
 public:
-TreeNode* DFS(TreeNode* root){
-    if(root==NULL)
-    return NULL;
-    TreeNode* L=DFS(root->left);
-    TreeNode* R=DFS(root->right);
-
-    root->left=R;
-    root->right=L;
-    return root;
+void BFS(TreeNode* root){
+    queue<TreeNode*>q;
+    q.push(root);
+    while(!q.empty()){
+        TreeNode* curr=q.front();
+        q.pop();
+        TreeNode* temp=curr->left;
+        curr->left=curr->right;
+        curr->right=temp;
+        if(curr->left!=NULL)
+        q.push(curr->left);
+        if(curr->right!=NULL)
+        q.push(curr->right);
+    }
 }
-    TreeNode* invertTree(TreeNode* root) {
+    TreeNode* invertTree(TreeNode* root) { 
         if(root==NULL)
         return NULL;
-        DFS(root);
+        BFS(root);
         return root;
+        
     }
 };
